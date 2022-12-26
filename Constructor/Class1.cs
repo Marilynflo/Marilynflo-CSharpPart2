dd91534134b4e4514bf9bd2ec8f900cd3e6adee0
mmf09 <mmf09@YPC>
2022-12-26 12:50:43 -0800

    Add project files.

---
+++ Constructor/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    public class Class1 // adding contructor to initialize the class
+    {
+        public string Name;
+        public int Acctcode;
+        public Class1()
+        {
+        }
+        public Class1(string userName)
+        {
+            this.Name = userName;
+        }
+        public Class1(string userName, int Acctcode)
+            :this(userName)
+        {
+            this.Acctcode = Acctcode;
+        }
+
+
+    }
+}
---
+++ Constructor/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string helloWorld = "Welcome"; // creating const variable
+            Console.WriteLine(helloWorld);
+
+            var user1 = new Class1("To Online Learning", 101); //Create an object of the class Class1
+            Console.WriteLine(user1.Name); //Print the value
+            Console.WriteLine(user1.Acctcode); //Print the value
+            Console.Read();
+        }
+    }
+}
